e8eca4737069b54874e37b4e8683f84d4d7a8a34
Alexandre Rinchet <alexandre.rinchet@etu.minesparis.psl.eu>
2026-01-06 16:13:42 +0100

    génération et affichage du graphe

--- app_fourmis.py
+++ app_fourmis.py
@@ -1,18 +1,72 @@
 import flet as ft
+import random
+import math
+
 
 def main(page : ft.Page) :
-    page.title = "ants algo"
+    page.title = "algo ants"
     page.theme_mode = ft.ThemeMode.LIGHT
     page.padding = 20
+    nodes = []
     titre = ft.Text("Visualisation de l'algorithme", size = 24, weight = "bold")
-    bouton = ft.Button("Cliquez-moi", on_click = lambda e : print("test"))
-    page.add(ft.Column([titre, bouton]))
-    champ1 = ft.TextField(label = "Nombre de noeuds", value = "20", width = 150)
-    champ2 = ft.TextField(label = "Nombre de fourmis", value = "15", width = 150)
-    champ3 = ft.TextField(label = "Nombre d'itérations", value = "100", width = 150)
+    champ_noeuds = ft.TextField(label = "Nombre de noeuds", value = "20", width = 150)
+    champ_fourmis = ft.TextField(label = "Nombre de fourmis", value = "15", width = 150)
+    champ_itérations = ft.TextField(label = "Nombre d'itérations", value = "100", width = 150)
     zgraph = ft.Container(width = 600, height = 500, bgcolor = "lightblue", border = ft.border.all(2, "blue"))
     titre2 = ft.Text("Paramètres de l'algorithme", size = 24, weight = "bold")
     statut = ft.Text("Prêt à démarrer", size = 16, color = "green")
-    page.add(ft.Column([titre2, ft.Row([champ1, champ2, champ3]), ft.Divider(), statut, zgraph]))
+
+    def generer_nodes() :
+        nonlocal nodes
+        try :
+            num_nodes = int(champ_noeuds.value)
+        except :
+            num_nodes = 20
+        nodes = []
+        for _ in range(num_nodes) :
+            x = random.uniform(50, 550)
+            y = random.uniform(50, 450)
+            nodes.append((x,y))
+        dessiner_graphe()
+
+        
+    
+    def dessiner_graphe() :
+        shapes = []
+        i = 1
+        for noeud in nodes :
+            x,y = noeud
+            circle = ft.Container(
+                width=20, height=20,
+                bgcolor="green",
+                border_radius=10,
+                left=x-10, top=y-10,
+                content=ft.Text(str(i), size=10, color="white"),
+                alignment=ft.Alignment(0,0)
+                )
+            shapes.append(circle)
+            i += 1
+        zgraph.content = ft.Stack(controls = shapes, width = 600, height = 500)
+        page.update()
+
+    btn_generer = ft.ElevatedButton(
+        "Générer le Graphe",
+        on_click=lambda e: generer_nodes()
+    )
+    
+    generer_nodes()
+
+    page.add(
+        ft.Column([
+            titre2,
+            ft.Row([champ_noeuds, champ_fourmis, champ_itérations]),
+            btn_generer,
+            ft.Divider(),
+            statut,
+            zgraph
+        ])
+    )
+    
+
 
 ft.run(main)
